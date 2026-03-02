5ee81cd20903301bddb77011ac99b63079a545b0
Keshav77463 <keshavsachdevarajp@gmail.com>
2026-03-02 18:41:03 +0530

    Add BNS section loading with metadata enrichment

--- src/ingest_qdrant.py
+++ src/ingest_qdrant.py
@@ -2,6 +2,8 @@ import os
 from dotenv import load_dotenv
 from qdrant_client import QdrantClient
 from qdrant_client.http.models import Distance, VectorParams
+import pandas as pd
+from langchain_community.document_loaders import TextLoader
 
 load_dotenv()
 
@@ -35,3 +37,27 @@ client.create_collection(
 )
 
 print("Collection created successfully.")
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+sections_meta_path = os.path.join(BASE_DIR, "data", "metadata", "bns_metadata.csv")
+sections_folder = os.path.join(BASE_DIR, "data", "Sections")
+
+df_sections = pd.read_csv(sections_meta_path)
+
+bns_documents = []
+
+for i in range(1, 359):
+    filepath = os.path.join(sections_folder, f"bns_section_{i}.txt")
+
+    loader = TextLoader(filepath, encoding="utf-8")
+    docs = loader.load()
+
+    meta_section = df_sections[df_sections["section"] == i]
+
+    docs[0].metadata["section"] = int(meta_section["section"].values[0])
+    docs[0].metadata["title"] = meta_section["title"].values[0]
+    docs[0].metadata["description"] = meta_section["description"].values[0]
+
+    bns_documents.append(docs[0])
+
+print("Documents loaded with metadata.")
