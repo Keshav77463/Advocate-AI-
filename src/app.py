825dc8e3511754892ecf8d5e77551ae59fb48986
Keshav77463 <keshavsachdevarajp@gmail.com>
2026-03-04 23:52:57 +0530

    Add Qdrant vector store integration for document retrieval

--- src/app.py
+++ src/app.py
@@ -1,4 +1,9 @@
 import streamlit as st
+from langchain_qdrant import QdrantVectorStore
+from qdrant_client import QdrantClient
+from langchain_community.embeddings import HuggingFaceEmbeddings
+import os
+from dotenv import load_dotenv
 
 st.set_page_config(page_title="Legal Assistant", page_icon="⚖️", layout="wide")
 
@@ -20,3 +25,20 @@ user_input = st.chat_input("Ask your legal question here...")
 if user_input:
     st.chat_message("user").write(user_input)
     st.session_state.messages.append({"role": "user", "content": user_input})
+    load_dotenv()
+
+    url = os.getenv("QDRANT_CLOUD_URL")
+    api_key = os.getenv("QDRANT_API_KEY")
+
+    dense_embedding = HuggingFaceEmbeddings(
+        model_name="sentence-transformers/all-MiniLM-L6-v2"
+    )
+
+    client = QdrantClient(url=url, api_key=api_key)
+
+    qdrant = QdrantVectorStore(
+        client=client,
+        collection_name="bns_sections",
+        embedding=dense_embedding,
+        vector_name="dense",
+    )
